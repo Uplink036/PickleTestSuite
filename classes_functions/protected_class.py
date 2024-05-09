3ff4a745294658545f23c2b2df89bb09cbfc6a9a
Tobias Mattsson <tomt21@student.bth.se>
2024-05-09 16:16:46 +0200

    added more functions for unittesting

---
+++ classes_functions/protected_class.py
@@ -0,0 +1,18 @@
+class ProtectedAttributes:
+    def __init__(self):
+        self._protected = "This is protected"
+
+    def __getattr__(self, name):
+        if name == "secret":
+            raise AttributeError("Access Denied")
+        return self.__dict__.get(name, f"{name} not found")
+
+    def __setattr__(self, name, value):
+        if name == "secret":
+            raise AttributeError("Cannot modify secret")
+        self.__dict__[name] = value
+
+    def __delattr__(self, name):
+        if name == "secret":
+            raise AttributeError("Cannot delete secret")
+        del self.__dict__[name]
