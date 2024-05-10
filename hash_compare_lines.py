cc24f369262f82ee4546e13254401eca0f050b30
Uplink036 <oliversjodin02@gmail.com>
2024-05-10 08:50:16 +0000

    Update random state

--- hash_compare_lines.py
+++ hash_compare_lines.py
@@ -7,9 +7,8 @@ import difflib
 
 ERRORS_FOUND = []
 
-def compare_hash(selected_file = None, protocol=pickle.DEFAULT_PROTOCOL):
+def compare_hash_lines(selected_file = None, sel_os = None, sel_py = None, protocol=pickle.DEFAULT_PROTOCOL):
     operatingsys_folder = os.listdir("logs")
-    operatingsys_folder = ["linux", "macOS"]
     with open(selected_file) as file_1: 
         file_1_text = file_1.readlines() 
     for os_folder in operatingsys_folder:
@@ -17,27 +16,36 @@ def compare_hash(selected_file = None, protocol=pickle.DEFAULT_PROTOCOL):
             current_file = f'logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv'
             with open(current_file) as file_2: 
                 file_2_text = file_2.readlines() 
-            for line in difflib.unified_diff( 
-                file_1_text, file_2_text, fromfile=selected_file,  
-                tofile=current_file, lineterm='\n'): 
-                global ERRORS_FOUND
-                ERRORS_FOUND.append([selected_file, current_file, line])
+            for line in file_2_text:
+                if line not in file_1_text:
+                    global ERRORS_FOUND
+                    ERRORS_FOUND.append([sel_os, sel_py, os_folder, pyversion, protocol, line])
+                    print(sel_os, sel_py, os_folder, pyversion, protocol, line)
 
 def compare_all(protocol=pickle.DEFAULT_PROTOCOL):
     operatingsys_folder = os.listdir("logs")
-    operatingsys_folder = ["linux", "macOS"]
     for os_folder in operatingsys_folder:
         for pyversion in os.listdir(f"logs/{os_folder}"):
-            compare_hash(f"logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv", protocol)
+            compare_hash_lines(f"logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv", os_folder, pyversion, protocol)
+            global ERRORS_FOUND
+            if ERRORS_FOUND == []:
+                print("No errors found")
+            else:
+                with open(f'hash_data_protocol_{protocol}.csv', 'w') as f:
+                    writer_object = csv.writer(f)
+                    headers = ["sel_os", "sel_py", "cur_os", "cur_py", "protocol", "diffhash"]
+                    writer_object.writerow(headers)
+                    writer_object.writerows(ERRORS_FOUND)
+                
+                ERRORS_FOUND = []
 
-    for error in ERRORS_FOUND:
-        print(error)
-
-    if ERRORS_FOUND == []:
-        print("No errors found")
+            print(pyversion)
 
 
 if __name__ == "__main__":
 
-    compare_all(3)
-    
+    for protocol in range(0, pickle.HIGHEST_PROTOCOL + 1):
+        try:
+            compare_all(protocol)
+        except:
+            pass
