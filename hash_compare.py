142d01f9033f0546ba67a074195ee332fdba9472
Uplink036 <oliversjodin02@gmail.com>
2024-05-10 07:25:49 +0000

    Update to better work with protocols

--- hash_compare.py
+++ hash_compare.py
@@ -3,24 +3,33 @@ import pickle
 import csv
 import pandas as pd
 
+ERRORS_FOUND = []
+
 def compare_hash(selected_file = None, protocol=pickle.DEFAULT_PROTOCOL):
     operatingsys_folder = os.listdir("logs")
     selected_df = pd.read_csv(selected_file)
     for os_folder in operatingsys_folder:
         for pyversion in os.listdir(f"logs/{os_folder}"):
-            current_df = pd.read_csv(f'logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv')
-            pd.testing.assert_frame_equal(selected_df, current_df, check_exact=True)
-
+            current_file = f'logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv'
+            current_df = pd.read_csv(current_file)
+            try: 
+                pd.testing.assert_frame_equal(selected_df, current_df)
+            except Exception as e:
+                global ERRORS_FOUND
+                ERRORS_FOUND.append((selected_file, current_file, e))
 
 
 def compare_all(protocol=pickle.DEFAULT_PROTOCOL):
     operatingsys_folder = os.listdir("logs")
     for os_folder in operatingsys_folder:
         for pyversion in os.listdir(f"logs/{os_folder}"):
-            compare_hash(f"logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv")
+            compare_hash(f"logs/{os_folder}/{pyversion}/protocol_{protocol}/data.csv", protocol)
+
+    for error in ERRORS_FOUND:
+        print(error)
 
 
 if __name__ == "__main__":
-    for protocol in range (0, pickle.HIGHEST_PROTOCOL):
-        compare_all(protocol)
+
+    compare_all(3)
     print("No errors found!")
