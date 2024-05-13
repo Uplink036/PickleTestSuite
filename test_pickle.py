5cb12e7fa95214781eca82006aa460f4b4026c32
Uplink036 <oliversjodin02@gmail.com>
2024-05-13 07:38:47 +0000

    Test with derandomizer

--- test_pickle.py
+++ test_pickle.py
@@ -9,7 +9,7 @@ from logger import save_unpickled_test,clean_folder
 from classes_functions import *
 from decimal import *
 
-settings.register_profile("first", max_examples=100, deadline=None)
+settings.register_profile("first", max_examples=100, deadline=None, derandomize=True)
 settings.load_profile("first")
 
 logger = True
@@ -52,7 +52,6 @@ def one_dim_random_strategy(draw):
 #     return ret
 
 class TestPickle:
-    @seed(1)
     @given(st.just(None))
     def test_none(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -74,7 +73,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="None", protocol=i)
 
-    @seed(2)
     @given(st.booleans())
     def test_booleans(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -96,7 +94,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(h_a1, comment="Boolean", protocol=i)
 
-    @seed(3)
     @given(st.just(Ellipsis))
     def test_ellipsis(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -118,7 +115,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Ellipsis", protocol=i)
 
-    @seed(4)
     @given(st.just(NotImplemented))
     def test_notimplemented(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -140,7 +136,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="NotImplemented", protocol=i)
 
-    @seed(5)
     @given(st.integers())
     def test_integers(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -162,7 +157,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Integer",json_data = str(data), protocol=i)
 
-    @seed(6)
     @given(st.floats(allow_nan=False))
     def test_floats(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -184,7 +178,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(h_a1, comment="Float",json_data = str(data), protocol=i)
 
-    @seed(7)
     @given(st.complex_numbers(allow_nan=False))
     def test_complex_numbers(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -206,7 +199,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Complex", protocol=i)
 
-    @seed(8)
     @given(st.text())
     def test_text(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -228,7 +220,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="String", protocol=i)
 
-    @seed(9)
     @given(st.binary())
     def test_binary(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -250,7 +241,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Bytes", protocol=i)
 
-    @seed(10)
     @given(st.lists(st.integers(min_value=0, max_value=255)))
     def test_bytearrays(self, data):
         c = bytearray(data)
@@ -273,7 +263,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Bytearray", protocol=i)
 
-    @seed(11)
     @given(st.tuples(st.integers(), st.floats(allow_nan=False), st.text(), st.booleans()))
     def test_tuples(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -295,7 +284,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Tuple", protocol=i)
 
-    @seed(12)
     @given(st.lists(elements=one_dim_random_strategy()))
     def test_lists(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -318,7 +306,6 @@ class TestPickle:
                 save_unpickled_test(data=h_a1, comment="List", protocol=i)
 
 
-    @seed(13)
     @given(st.sets(elements=one_dim_random_strategy()))
     def test_sets(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -340,7 +327,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Set", protocol=i)
 
-    @seed(14)
     @given(st.dictionaries(keys=st.text(), values=one_dim_random_strategy()))
     def test_dictionaries(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -362,7 +348,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Dictionary", protocol=i)
 
-    @seed(15)
     @given(st.recursive(one_dim_random_strategy(), lambda children: st.lists(children)))
     def test_recursive_lists(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -384,7 +369,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Recursive List", protocol=i)
 
-    @seed(16)
     @given(st.recursive(one_dim_random_strategy(), lambda children: st.frozensets(children)))
     def test_recursive_sets(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -406,7 +390,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Recursive Set", protocol=i)
 
-    @seed(17)
     @given(st.recursive(one_dim_random_strategy(), lambda children: st.dictionaries(keys=st.text(), values=children)))
     def test_recursive_dictionaries(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -426,8 +409,8 @@ class TestPickle:
 
             assert h_a1 == h_a2
             if logger:
-                save_unpickled_test(data=h_a1, comment="Recursive Dictionary", protocol=i)
-    @seed(18)
+                save_unpickled_test(data=h_a1, comment="Recursive Dictionary", protocol=i))
+    
     @given(st.sampled_from([add_function, is_prime, tri_recursion, get_lengths]))
     def test_functions(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -447,8 +430,8 @@ class TestPickle:
 
             assert h_a1 == h_a2
             if logger:
-                save_unpickled_test(data=h_a1, comment="Function", protocol=i)
-    @seed(19)
+                save_unpickled_test(data=h_a1, comment="Function", protocol=i))
+    
     @given(st.sampled_from([Student, Person, Temperature]))
     def test_classes(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
@@ -470,7 +453,6 @@ class TestPickle:
             if logger:
                 save_unpickled_test(data=h_a1, comment="Class", protocol=i)
 
-    @seed(20)
     @given(st.sampled_from([Student("Tobias", 21), Person("Oliver", 21), Temperature(37)]))
     def test_instances(self, data):
         for i in range(0, pickle.HIGHEST_PROTOCOL + 1):
